64958dff88196649c64aa1fe84f3665845255780
danilobrayann <danilobrayann@gmail.com>
2025-09-18 14:18:44 -0300

    refactor: Corrige formatação e adiciona inicialização de rotas

--- router/router.go
+++ router/router.go
@@ -3,13 +3,11 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initilizer() {
-	router := gin.Default()
+	router  := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello, Gin!",
-		})
-	})
+      // inicilaze routes
 
+	 
+	// run the serve
 	router.Run(":8080") // roda em http://localhost:8080
 }
